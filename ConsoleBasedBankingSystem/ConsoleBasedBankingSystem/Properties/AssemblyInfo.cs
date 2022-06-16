32bd9e4da5bfc0c6d0a84fc2d059fa1a6f67058d
ramyagollapalli2000 <107127021+ramyagollapalli2000@users.noreply.github.com>
2022-06-16 01:38:04 -0700

    ConsoleBasedBankingSystem

--- ConsoleBasedBankingSystem/ConsoleBasedBankingSystem/Properties/AssemblyInfo.cs
+++ ConsoleBasedBankingSystem/ConsoleBasedBankingSystem/Properties/AssemblyInfo.cs
@@ -24,8 +24,8 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version
+//      Major version
+//      Minor version
 //      Build Number
 //      Revision
 //
